46372514fddde2dfdcebca0ef4942522033ff973
Katia Duarte <katiaipduarte@hotmail.com>
2022-08-23 19:41:28 +0100

    feat: add reports module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '@core/core.module';
+import { CustomMaterialModule } from '@custom-material/custom-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomMaterialModule } from './custom-material/custom-material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CustomMaterialModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, CustomMaterialModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
